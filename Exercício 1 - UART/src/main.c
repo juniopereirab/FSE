64076100723c082fff0f3e51ff5344bca291885d
Damaso Junio <junio.pereirab@gmail.com>
2021-08-12 09:54:57 -0300

    update main uart 1

--- Exercício 1 - UART/src/main.c
+++ Exercício 1 - UART/src/main.c
@@ -14,11 +14,16 @@ int main(int argc, const char * argv[]) {
     unsigned char *p_tx_buffer;
     
     p_tx_buffer = &tx_buffer[0];
-    *p_tx_buffer++ = 'H';
-    *p_tx_buffer++ = 'e';
+    *p_tx_buffer++ = '163';
+    *p_tx_buffer++ = '3';
+    *p_tx_buffer++ = 'O';
     *p_tx_buffer++ = 'l';
-    *p_tx_buffer++ = 'l';
-    *p_tx_buffer++ = 'o';
+    *p_tx_buffer++ = 'a';
+    *p_tx_buffer++ = '1';
+    *p_tx_buffer++ = '4';
+    *p_tx_buffer++ = '3';
+    *p_tx_buffer++ = '8';
+    
 
     printf("Buffers de memória criados!\n");
     
