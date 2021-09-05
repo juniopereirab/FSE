6809f0248101877fe2e460876433588a044b2b0d
Damaso Junio <junio.pereirab@gmail.com>
2021-09-05 18:46:44 -0300

    Send and Receive functions on Uart implemented

--- Trabalho1/src/uart.c
+++ Trabalho1/src/uart.c
@@ -6,6 +6,7 @@
 #include <unistd.h>   //Used for UART
 
 #include "uart_defs.h"
+#include "crc16.h"
 
 int initUart(){
     int uart_filestream = -1;
@@ -29,16 +30,59 @@ int initUart(){
 }
 
 void requestToUart(int uart_filestream, unsigned char code){
-    return;
+    unsigned char package[7] = {0x01, 0x23, code, 0x01, 0x04, 0x03, 0x08};
+    short crc = calcula_CRC(package, 7);
+
+    unsigned char message[9];
+    memcpy(message, &package, 7);
+    memcpy(&message[7], &crc, 2);
+    int check = write(uart_filestream, &message[0], 9);
+    if(check < 0){
+        printf("Ocorreu um erro na comunicação com o UART\n");
+    }
+    sleep(1);
 }
 
 void sendToUart(int uart_filestream, int value){
-    return;
+    unsigned char package[7] = {0x01, 0x16, SEND_SIGNAL, 0x01, 0x04, 0x03, 0x08};
+    unsigned char message[13];
+
+    memcpy(message, &package, 7);
+    memcpy(&message[7], &value, 4);
+
+    short crc = calcula_CRC(message, 11);
+
+    memcpy(&message[11], &crc, 2);
+
+    int check = write(uart_filestream, &message[0], 13);
+
+    if(check < 0){
+        printf("Ocorreu um erro na comunicação com o UART\n");
+    }
+    sleep(1);
 }
 
 Number_type readFromUart(int uart_filestream, unsigned char code){
+    unsigned char buffer[20];
     Number_type number = {-1, -1.0};
 
+    int content = read(uart_filestream, buffer, 20);
+    if(!content){
+        printf("Nenhum dado foi recebido\n");
+    }
+    else if(content < 0){
+        printf("Erro ao ler dados\n");
+    }
+    else {
+        buffer[content] = '\0';
+        if (code == 0xC3){
+            memcpy(&number.int_value, &buffer[3], sizeof(int));
+        }
+        else{
+            memcpy(&number.float_value, &buffer[3], sizeof(float));
+        }
+        return number;
+    }
     return number; 
 }
 
