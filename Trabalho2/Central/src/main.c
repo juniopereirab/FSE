9f342d54380c7d5c3ab8dce89cdbffbad3c10732
Damaso Junio <junio.pereirab@gmail.com>
2021-10-06 15:52:44 -0300

    create main =)

--- Trabalho2/Central/src/main.c
+++ Trabalho2/Central/src/main.c
@@ -1,3 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "application.h"
+#include "socketKiller.h"
+#include "mainMenu.h"
+#include "gpio.h"
+#include "tcpServer.h"
+#include "csv.h"
+
 int main() {
-    return 0;
+  quit_setup();
+  csv_setup();
+  app_init();
+
+  pthread_t tid[3];
+
+  pthread_create(&tid[0], NULL, (void *)appHandler, (void *)NULL);
+  pthread_create(&tid[1], NULL, (void *)menuHandler, (void *)NULL);
+  pthread_create(&tid[2], NULL, (void *)recvMessage, (void *)NULL);
+
+  pthread_join(tid[0], NULL);
+  pthread_join(tid[1], NULL);
+  pthread_join(tid[2], NULL);
+
+  return 0;
 }
