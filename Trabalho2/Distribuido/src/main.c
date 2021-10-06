875756b46e95c666b0a1c0e90f43f65fe28c5e57
Damaso Junio <junio.pereirab@gmail.com>
2021-10-06 00:02:03 -0300

    Add main handler from distributed

--- Trabalho2/Distribuido/src/main.c
+++ Trabalho2/Distribuido/src/main.c
@@ -1,3 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include "tcpServer.h"
+#include "socketKiller.h"
+#include "gpio.h"
+#include "dht22.h"
+
 int main() {
+    gpioSetup();
+    killSetup();
+    dht22_setup();
+    turnDevicesOff();
+
+    pthread_t tid[4];
+
+    pthread_create(&tid[0], NULL, (void*)gpioHandler, (void*) NULL);
+    pthread_create(&tid[1], NULL, (void*)peopleQuantityHandler, (void*) NULL);
+    pthread_create(&tid[2], NULL, (void*)serverHandler, (void*) 0);
+    pthread_create(&tid[3], NULL, (void*)serverHandler, (void*) 1);
+
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+    pthread_join(tid[2], NULL);
+    pthread_join(tid[3], NULL);
+    
     return 0;
 }
